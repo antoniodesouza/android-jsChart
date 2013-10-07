28c653bc768db4eb0cdd0745f9df7fc4dc578fd0
fan543 <fan543119@gmail.com>
2013-10-07 21:02:54 +0800

    Android js chart

---
+++ chart/src/com/example/chart/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.chart;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		  super.loadUrl(Config.getStartUrl(),3000);
+	}
+
+
+}
